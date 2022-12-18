3ff8f70a5ae9438931751fd66bb0d3b6b93f819c
ldiazriod <luis.diaz@langoo.io>
2022-12-18 12:32:05 +0100

    new integrations

--- src/components/MainMenu/PublicCubes/PublicCubes.tsx
+++ src/components/MainMenu/PublicCubes/PublicCubes.tsx
@@ -7,6 +7,10 @@ import parse from "html-react-parser";
 import {MainDivRe} from "../../../styles/globalStyles";
 import TextArea from "../../others/TextArea";
 
+type Props = {
+    creator: string
+}
+
 type SearchValues = {
     cardMainTitle?: string | undefined,
     cubeDimensions?: string | undefined,
@@ -30,6 +34,17 @@ type CubeInfo = {
     public?: boolean
 }
 
+const defaultCubeState = {
+    creator: "",
+    cubeName: "",
+    cubeDimensions: "",
+    cardMainTitle: "",
+    cardText: "",
+    cardReviewPoints: 0,
+    cardImg: "",
+    public: false
+}
+
 const SEARCH_PUBLIC_CUBES = gql`
     query getPublicCubes($page: Int!, $search: PublicSearch){
         getPublicCubes(page: $page, search: $search){
@@ -48,15 +63,25 @@ const SEARCH_PUBLIC_CUBES = gql`
         }
     }
 `
-const PublicCubes = (): JSX.Element => {
+const PublicCubes = (props: Props): JSX.Element => {
     const [searchValues, setSeachValues] = useState<SearchValues>()
     const [page, setPage] = useState<number>(1)
+    const [modal, setModal] = useState<boolean>(false)
+    const [cubeInfo, setCubeInfo] = useState<CubeInfo>({...defaultCubeState, creator: props.creator})
     const {data, error, loading, refetch} = useQuery<{getPublicCubes: CubeInfo[]}>(SEARCH_PUBLIC_CUBES, {
         variables: {
             page: page,
             search: searchValues
         }
     })
+
+    const openModal = () => {
+        setModal(true)
+    }
+    const closeModal = () => {
+        setModal(false)
+    }
+
     useEffect(() => {
         refetch()
     }, [page, searchValues])
@@ -69,12 +94,36 @@ const PublicCubes = (): JSX.Element => {
     }
     return (
         <div>
+            <Modal
+                isOpen={modal}
+                onRequestClose={closeModal}
+                contentLabel="Cube card"
+            >
+                <strong>{cubeInfo.cardMainTitle}</strong>
+                <span>{parse(cubeInfo.cardText)}</span>
+                {props.creator !== cubeInfo.creator && 
+                    <button>Clone</button>
+                }
+            </Modal>
             <input type="text" placeholder="Title" value={searchValues?.cardMainTitle} onChange={(e) => setSeachValues(searchValues ? {...searchValues, cardMainTitle: e.target.value} : {cardMainTitle: e.target.value})}/>
-            {data && data.getPublicCubes.map((elem) => {
-                return <div>
-                    {elem.cardMainTitle}
-                </div>
-            })}
+            <label htmlFor="dropdown">Select points:</label>
+            <select name="dropdown" id="dropdown" onChange={(e) => setSeachValues(searchValues ? {...searchValues, cardReviewPoints: Number(e.target.value)} : {cardReviewPoints: Number(e.target.value)})}>
+                <option value={5}>5 points</option>
+                <option value={4}>4 points</option>
+                <option value={3}>3 points</option>
+                <option value={2}>2 points</option>
+                <option value={1}>1 points</option>
+                <option value={0}>0 points</option>
+            </select>
+            <CubeWrapper>
+                {data && data.getPublicCubes.map((elem) => {
+                    return <SingleCubeCard onClick={() => [setCubeInfo(elem), openModal()]}>
+                        <strong>{elem.cardMainTitle}</strong>
+                        <CreatorButton>{elem.creator}</CreatorButton>
+                        <CardImg src={elem.cardImg} alt={`${elem.cubeName} img`}/>
+                    </SingleCubeCard>
+                })}
+            </CubeWrapper>
             {page > 1 && <button onClick={() => setPage(page-1)}>Prev</button>}
             <button onClick={() => setPage(page+1)}>Next</button>
         </div>
@@ -82,3 +131,71 @@ const PublicCubes = (): JSX.Element => {
 }
 
 export default PublicCubes;
+
+const CubeWrapper: StyledComponent<"div", any, {}, never> = styled.div`
+    display: grid;
+    align-items: center;
+    justify-items: center;
+    grid-template-columns: repeat(3, 1fr);    
+    width: 100%;
+    height: 100%;
+`
+const SingleCubeCard: StyledComponent<"div", any, {}, never> = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 300px;
+    height: auto;
+    background: white;
+    box-shadow: 0px 5px 5px #97949496;
+    border-radius: 8px;
+    padding: 10px;
+    margin: 20px;
+`
+const CardImg: StyledComponent<"img", any, {}, never> = styled.img`
+    width: 90%;
+    height: 200px;
+`
+const CreatorButton = styled.button`
+    background: transparent;
+    border: transparent;
+    border-bottom: 1px solid black;
+`
+const FinishButton = styled.button`
+    background: #b31860;
+    border: 2px solid #b31860;
+    color: white;
+    width: 20%;
+    padding: 10px 25px 10px 25px;
+    margin-top: 20px;
+    margin-bottom: 20px;
+    box-shadow: 0px 5px 5px #97949496;
+    border-radius: 8px;
+    font-size: 16px;
+    font-weight: 700;
+    transition: 0.45s;
+    &:hover {
+        border: 2px solid #b31860;
+        color: #b31860;
+        background: white;
+    }
+`
+const GoBackButton = styled.button<{state: boolean}>`
+    background: ${props => props.state ? "white" : "#b31860"};
+    border: 2px solid #b31860;
+    color: ${props => props.state ? "#b31860" : "white" };
+    width: 20%;
+    padding: 10px 25px 10px 25px;
+    margin-top: 20px;
+    margin-bottom: 20px;
+    box-shadow: 0px 5px 5px #97949496;
+    border-radius: 8px;
+    font-size: 16px;
+    font-weight: 700;
+    transition: 0.45s;
+    &:hover {
+        border: 2px solid #b31860;
+        color: ${props => props.state ? "white" : "#b31860"};
+        background: ${props => props.state ? "#b31860" : "white"};
+    }
+`
